32c57c8f163e0200c0d35e8772069b1feef13bc9
Sergio L. Benítez D <sl.benitezd@suabochica.com>
2018-05-18 10:28:14 -0500

    Solving mental maths game exercise

---
+++ react_fundamentals/03_state_management/exercises/mental_maths_game/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './Game';
+import Score from './Score';
+
+class App extends Component {
+  state = {
+    score: 0,
+    correctAnswer: 0,
+    numQuestions: 0
+  }
+
+  handleAnswer = (answerWasCorrect) => {
+    if (answerWasCorrect) {
+      this.setState(prevState => ({
+        correctAnswer: prevState.correctAnswer + 1
+      }));
+    }
+
+    this.setState(prevState => ({
+      numQuestions: prevState.numQuestions + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <Game
+            handleAnswer={this.handleAnswer}
+          />
+          <Score
+            numCorrect={this.state.correctAnswer}
+            numQuestions={this.state.numQuestions}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
