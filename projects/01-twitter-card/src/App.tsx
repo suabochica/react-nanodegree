9ec4652a5710d396dcd72604d1ce52b4c1fa61ab
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-01-17 07:08:31 -0500

    react: add button foundations and twitter card component.

---
+++ projects/01-twitter-card/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://suabochica.com/sua_profile.jpg" alt="Avatar" />
+                <div>
+                    <strong>Sergio Leonardo Benítez</strong>
+                    <span>@suabocihca</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
