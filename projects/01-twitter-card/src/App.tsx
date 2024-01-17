b2d4ded8786881cfdcf0018561b883a1a4c78659
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-01-17 14:48:05 -0500

    react: use swift name convention fro styles.

--- projects/01-twitter-card/src/App.tsx
+++ projects/01-twitter-card/src/App.tsx
@@ -2,17 +2,21 @@ import './App.css'
 
 export function App() {
     return (
-        <article>
-            <header>
-                <img src="https://suabochica.com/sua_profile.jpg" alt="Avatar" />
-                <div>
-                    <strong>Sergio Leonardo Benítez</strong>
-                    <span>@suabocihca</span>
+        <article className='tw-follow-card'>
+            <header className='tw-follow-card-header'>
+                <img
+                    className='tw-follow-card-avatar'
+                    src="https://suabochica.com/sua_profile.jpg"
+                    alt="Avatar"
+                />
+                <div className='tw-follow-card-info'>
+                    <strong className='tw-follow-card-name'>Sergio Leonardo Benítez</strong>
+                    <span className='tw-follow-card-tag'>@suabocihca</span>
                 </div>
             </header>
 
             <aside>
-                <button>Seguir</button>
+                <button className='tw-follow-card-button'>Seguir</button>
             </aside>
         </article>
     )
