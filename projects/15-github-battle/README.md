9db9630cf3c54ab489fe8d5a8e1ff61cfdf78538
Sergio Benítez <sergio.kun21@gmail.com>
2026-05-13 20:16:58 -0500

    project:  add initial setup for github battle.

---
+++ projects/15-github-battle/README.md
@@ -0,0 +1,5 @@
+# GitHub Battle
+
+This project is a "Github Battle" app. You'll be able to see the most popular repos for a variety of languages as well as battle two Github users to see who has the better profile.
+
+You can view the final project at github-battle.ui.dev/
---
+++ projects/15-github-battle/app/components/Nav.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import ThemeContext from "../contexts/theme";
+
+const activeStyle = {
+  color: "rgb(187, 46, 31)",
+};
+
+export default function Nav({ toggleTheme }: { toggleTheme: () => void }) {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink to="/" exact activeStyle={activeStyle} className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/battle" activeStyle={activeStyle} className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? "🔦" : "💡"}
+      </button>
+    </nav>
+  );
+}
