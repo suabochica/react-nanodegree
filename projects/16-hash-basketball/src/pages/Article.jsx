dd08ddfda20b78b19f36d25591de3b91f165a37d
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2026-06-25 11:15:27 -0500

    hash basketball: add Navigate to programmatically navigation

--- projects/16-hash-basketball/src/pages/Article.jsx
+++ projects/16-hash-basketball/src/pages/Article.jsx
@@ -1,25 +1,26 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Navigate } from "react-router-dom";
 
-import useArticle from '../hooks/useArticle'
-import Loading from '../components/Loading'
+import useArticle from "../hooks/useArticle";
+import Loading from "../components/Loading";
 
-export default function Article () {
-  const { teamId, articleId } = useParams()
-  const {
-    response: article,
-    loading
-  } = useArticle({ teamId, articleId })
-  if (loading) return null
-  return (
-    <div className='panel'>
-      { loading
-        ? <Loading />
-        : (
-          <article className='article'>
-            <h1 className='header'>{article.title}</h1>
-            <p>{article.body}</p>
-          </article>
-        )}
-    </div>  
-  )
+export default function Article() {
+  const { teamId, articleId } = useParams();
+  const { response: article, loading } = useArticle({ teamId, articleId });
+
+  let body;
+
+  if (loading === true) {
+    body = <Loading />;
+  } else if (article === null) {
+    body = <Navigate to={`/${teamId}/articles`} />;
+  } else {
+    body = (
+      <article className="article">
+        <h1 className="header">{article.title}</h1>
+        <p>{article.body}</p>
+      </article>
+    );
+  }
+
+  return <div className="panel">{body}</div>;
 }
--- projects/16-hash-basketball/src/pages/Team.jsx
+++ projects/16-hash-basketball/src/pages/Team.jsx
@@ -1,96 +1,40 @@
-import { useParams, Link } from "react-router-dom";
+import { Link, useParams, Navigate } from "react-router-dom";
 
-import useTeamNames from "../hooks/useTeamNames";
-import useTeamsArticles from "../hooks/useTeamsArticles";
 import useTeam from "../hooks/useTeam";
-
-import { slugify } from "../utils";
-
 import TeamLogo from "../components/TeamLogo";
 import Loading from "../components/Loading";
 
-function useTeamPageData(teamId) {
-  const { response: teamNames, loading: loadingTeamNames } = useTeamNames();
-
-  const { response: articles, loading: loadingArticles } =
-    useTeamsArticles(teamId);
-
-  const { response: team, loading: loadingTeam } = useTeam(teamId);
-
-  return {
-    teamNames,
-    articles,
-    team,
-    loading: loadingArticles || loadingTeamNames || loadingTeam,
-  };
-}
-
-export default function TeamPage() {
+export default function Team() {
   const { teamId } = useParams();
+  const { loading, response: team } = useTeam(teamId);
 
-  const { teamNames, articles, team, loading } = useTeamPageData(teamId);
-
+  let body;
   if (loading === true) {
-    return <p>LOADING</p>;
-  }
-
-  if (!teamNames.includes(teamId)) {
-    return <h1 className="text-center">The {teamId} is not a valid team.</h1>;
-  }
-
-  return (
-    <div className="panel">
-      {loading ? (
-        <Loading />
-      ) : (
-        <div style={{ width: "100%" }}>
-          <TeamLogo id={teamId} />
-          <h1 className="medium-header">{team.name}</h1>
-          <h4 style={{ margin: 5 }}>
-            <Link to={{ pathname: "/players", search: `?teamId=${teamId}` }}>
-              View Roster
-            </Link>
-          </h4>
-        </div>
-      )}
-      <h4>Championships</h4>
-      <ul className="championships">
-        {team.championships.map((ship) => (
-          <li key={ship}>{ship}</li>
-        ))}
-      </ul>
-      <ul className="info-list row" style={{ width: "100%" }}>
-        <li>
-          Est.<div>{team.established}</div>
-        </li>
-        <li>
-          Manager<div>{team.manager}</div>
-        </li>
-        <li>
-          Coach<div>{team.coach}</div>
-        </li>
-        <li>
-          Record
-          <div>
-            {team.wins}-{team.losses}
-          </div>
-        </li>
-      </ul>
-      <h2 className="header">Articles</h2>
-      <ul className="articles">
-        {articles.map((article) => (
-          <li key={article.id}>
-            <h4 className="article-title">
-              <Link to={`/${teamId}/articles/${slugify(article.title)}`}>
-                {article.title}
-              </Link>
-            </h4>
-            <div className="article-date">
-              {new Date(article.date).toLocaleDateString()}
-            </div>
+    body = <Loading />;
+  } else if (team === null) {
+    body = <Navigate to="/teams" />;
+  } else {
+    body = (
+      <div style={{ width: "100%" }}>
+        <TeamLogo id={team.id} className="center" />
+        <h1 className="medium-header">{team.name}</h1>
+        <ul className="info-list row">
+          <li>
+            Est.<div>{team.established}</div>
           </li>
-        ))}
-      </ul>
-    </div>
-  );
+          <li>
+            Manager<div>{team.manager}</div>
+          </li>
+          <li>
+            Coach<div>{team.coach}</div>
+          </li>
+        </ul>
+        <Link className="center btn-main" to={`/${teamId}`}>
+          {team.name} Team Page
+        </Link>
+      </div>
+    );
+  }
+
+  return <div className="panel">{body}</div>;
 }
